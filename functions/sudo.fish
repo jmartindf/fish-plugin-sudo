aad10022337b73b15af051e0026e9e588408c139
Joe Martin <joe@desertflood.com>
2025-06-28 16:22:07 -0700

    fix: error running sudo
    Signed-off-by: Joe Martin <joe@desertflood.com>

--- functions/sudo.fish
+++ functions/sudo.fish
@@ -4,9 +4,10 @@
 # Copyright (C) 2025 jmartindf (if block)
 # -----------------------------------------------------------------------------
 function sudo
+    set -l real_sudo (command -s sudo)
     if contains -- -i $argv
-        command sudo $argv
+        $real_sudo $argv
     else
-        env SHELL=(which fish) command sudo -sE $argv
+        env SHELL=(which fish) $real_sudo -sE $argv
     end
 end
